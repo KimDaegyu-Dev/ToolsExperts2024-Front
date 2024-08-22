f274bfae5040cf2f89fc3f52d363f6558bf39b58
KimDaegyu-Dev <136952602+KimDaegyu-Dev@users.noreply.github.com>
2024-08-22 12:30:48 +0900

    Fix: 충돌 에러

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import FollowPage from './followPage';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import FollowPage from "./followPage";
 //import MynearPage from './myNearPage';
 //import InfoPage from './infoPage';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <FollowPage />
+    <DetailPage />
   </React.StrictMode>
 );
 
