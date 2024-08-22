ea51c9a235228b216c65dd1a3d13f3d82d72bd79
seoan <sakim9018@naver.com>
2024-08-22 12:25:27 +0900

    Fix: 폴더 디렉토리 수정

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-//import FollowPage from './followPage';
+import FollowPage from './followPage';
 //import MynearPage from './myNearPage';
-import InfoPage from './infoPage';
+//import InfoPage from './infoPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <InfoPage />
+    <FollowPage />
   </React.StrictMode>
 );
 
