3b7ed95510fb5cb5e2ae14c33097e708d89d5b1a
seoan <sakim9018@naver.com>
2024-08-22 09:45:55 +0900

    내 근처 페이지, 관람정보 페이지 완료
    내 근처 페이지, 관람정보 페이지 완료했습니다.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import FollowPage from './followPage';
+//import FollowPage from './followPage';
+//import MynearPage from './myNearPage';
+import InfoPage from './infoPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <FollowPage />
+    <InfoPage />
   </React.StrictMode>
 );
 
