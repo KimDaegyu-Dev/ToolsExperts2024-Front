d61acba17d0595cacdbd485639168f6ae03e5037
ynyejin <yyj030625@gmail.com>
2024-11-22 20:24:41 +0900

    수정

--- frontend/src/pages/MyPage.js
+++ frontend/src/pages/MyPage.js
@@ -5,36 +5,40 @@ import roundHeart from "../assets/roundHeart.png";
 function MyPage() {
   return (
     <>
-      <div className="container">
+      <div className="container1">
         <p>
-          <p className="circle"></p>
-          <span className="name">김민주님 안녕하세요!</span>
+          <p className="circle1"></p>
+          <span className="name1">김민주님 안녕하세요!</span>
         </p>
-        <p className="myInfo">내 정보</p>
+        <a href="#" className="myInfo">
+          내 정보
+        </a>
         <div className="attendedContainer">
           <p className="attended-text">이번 달에 관람한 전시회</p>
           <p className="attended-num">
             총 <span>7</span>개
           </p>
-          <div className="max"></div>
-          <div className="current"></div>
+          <div className="max1"></div>
+          <div className="current1"></div>
           <p className="zero">0개</p>
           <p className="ten">10개</p>
         </div>
         <div className="myPage">마이페이지</div>
-        <div className="BookmarkBox">
-          <img className="roundHeart" src={roundHeart} alt="roundHeart" />
-          <p>내가 찜해놓은 전시</p>
-        </div>
-        <div className="PreviousReviewBox">
-          <img className="roundHeart" src={roundHeart} alt="roundHeart" />
-          <p>지난 리뷰 작성</p>
-        </div>
-        <div className="MyReviewBox">
-          <img className="roundHeart" src={roundHeart} alt="roundHeart" />
-          <p>내가 남긴 리뷰</p>
-        </div>
-        <div className="logout">로그아웃</div>
+        <a href="#" className="BookmarkBox">
+          <img className="roundHeart1" src={roundHeart} alt="roundHeart" />
+          <div>내가 찜해놓은 전시</div>
+        </a>
+        <a href="#" className="PreviousReviewBox">
+          <img className="roundHeart1" src={roundHeart} alt="roundHeart" />
+          <div>지난 리뷰 작성</div>
+        </a>
+        <a href="#" className="MyReviewBox">
+          <img className="roundHeart1" src={roundHeart} alt="roundHeart" />
+          <div>내가 남긴 리뷰</div>
+        </a>
+        <a href="#" className="logout">
+          로그아웃
+        </a>
       </div>
     </>
   );
