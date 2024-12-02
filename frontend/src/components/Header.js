22f62e4046721e125303dd458584605d3f641d71
ynyejin <yyj030625@gmail.com>
2024-12-03 00:53:36 +0900

    헤더 수정

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,67 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import "../components/Header.css";
 import search from "../assets/search.png";
 import heart from "../assets/heart.png";
 import profile from "../assets/profile.png";
+import MyPage from "../pages/MyPage";
 import { Link, useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const [isMyPageOpen, setMyPageOpen] = useState(false);
   const navigate = useNavigate();
+
+  const handleSearchFocus = () => {
+    setMyPageOpen(false);
+    removeRipple();
+  };
+
+  const removeRipple = () => {
+    const profileCircle = document.querySelector(".profile-circle");
+    const rippleExists = profileCircle?.querySelector(".ripple");
+    if (rippleExists) rippleExists.remove();
+  };
+
+  const handleProfileClick = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    const profileCircle = e.currentTarget;
+    const rippleExists = profileCircle.querySelector(".ripple");
+
+    if (rippleExists) {
+      rippleExists.remove();
+    } else {
+      const ripple = document.createElement("span");
+      ripple.classList.add("ripple");
+      profileCircle.appendChild(ripple);
+    }
+
+    setMyPageOpen((prev) => !prev);
+  };
+
+  const handleNavigation = (path) => {
+    setMyPageOpen(false);
+    removeRipple();
+    navigate(path);
+  };
+
   return (
     <div>
       <header className="header">
         <Link to="/Landing" className="logo"></Link>
         <ul>
           <li>
-            <span
-              onClick={() => {
-                navigate("/detail");
-              }}
-            >
-              전시 추천
-            </span>
+            <span onClick={() => handleNavigation("/landing")}>전시 추천</span>
           </li>
           <li>
-            <span
-              onClick={() => {
-                navigate("/detail");
-              }}
-            >
-              팔로우
-            </span>
+            <span onClick={() => handleNavigation("/detail")}>팔로우</span>
           </li>
           <li>
-            <span
-              onClick={() => {
-                navigate("/detail");
-              }}
-            >
-              내 근처
-            </span>
+            <span onClick={() => handleNavigation("/mynear")}>내 근처</span>
           </li>
           <li>
-            <span
-              onClick={() => {
-                navigate("/schedule");
-              }}
-            >
-              일정별
-            </span>
+            <span onClick={() => handleNavigation("/schedule")}>일정별</span>
           </li>
         </ul>
-        <a href="#">
-          <div className="search"></div>
-        </a>
-        <a href="#">
+        <form className="search">
+          <input
+            className="search-text"
+            type="text"
+            spellCheck="false"
+            onFocus={handleSearchFocus}
+          />
+        </form>
+        <a>
           <img className="search-icon" src={search} alt="search" />
         </a>
-        <a href="#">
+        <a>
           <img className="heart-icon" src={heart} alt="heart" />
         </a>
-        <a href="#">
+        <a className="profile-circle" href="#" onClick={handleProfileClick}>
           <img className="profile-icon" src={profile} alt="profile" />
         </a>
       </header>
+      {isMyPageOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <MyPage />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- frontend/src/pages/MyPage.js
+++ frontend/src/pages/MyPage.js
@@ -5,39 +5,39 @@ import roundHeart from "../assets/roundHeart.png";
 function MyPage() {
   return (
     <>
-      <div className="container1">
-        <p>
-          <span className="name1">김민주님 안녕하세요!</span>
-        </p>
-        <a href="#" className="myInfo">
-          내 정보
-        </a>
-        <div className="attendedContainer">
-          <p className="attended-text">이번 달에 관람한 전시회</p>
-          <p className="attended-num">
-            총 <span>7</span>개
-          </p>
-          <div className="max1"></div>
-          <div className="current1"></div>
-          <p className="zero">0개</p>
-          <p className="ten">10개</p>
+      <div className="modal">
+        <div className="container1">
+          <div className="Name">김민주님 안녕하세요!</div>
+          <a href="#" className="myInfo">
+            내 정보
+          </a>
+          <div className="attendedContainer">
+            <p className="attended-text">이번 달에 관람한 전시회</p>
+            <p className="attended-num">
+              총 <span>7</span>개
+            </p>
+            <div className="max1"></div>
+            <div className="current1"></div>
+            <p className="zero">0개</p>
+            <p className="ten">10개</p>
+          </div>
+          <div className="myPage">마이페이지</div>
+          <a href="#" className="BookmarkBox">
+            <img className="roundHeart1" src={roundHeart} alt="roundHeart" />
+            <div>내가 찜해놓은 전시</div>
+          </a>
+          <a href="#" className="PreviousReviewBox">
+            <img className="roundHeart1" src={roundHeart} alt="roundHeart" />
+            <div>지난 리뷰 작성</div>
+          </a>
+          <a href="#" className="MyReviewBox">
+            <img className="roundHeart1" src={roundHeart} alt="roundHeart" />
+            <div>내가 남긴 리뷰</div>
+          </a>
+          <a href="#" className="logout">
+            로그아웃
+          </a>
         </div>
-        <div className="myPage">마이페이지</div>
-        <a href="#" className="BookmarkBox">
-          <img className="roundHeart1" src={roundHeart} alt="roundHeart" />
-          <div>내가 찜해놓은 전시</div>
-        </a>
-        <a href="#" className="PreviousReviewBox">
-          <img className="roundHeart1" src={roundHeart} alt="roundHeart" />
-          <div>지난 리뷰 작성</div>
-        </a>
-        <a href="#" className="MyReviewBox">
-          <img className="roundHeart1" src={roundHeart} alt="roundHeart" />
-          <div>내가 남긴 리뷰</div>
-        </a>
-        <a href="#" className="logout">
-          로그아웃
-        </a>
       </div>
     </>
   );
